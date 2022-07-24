d55f8d39e3f3e09670ced17e4d0fb53fbf0cfa5d
hamidhere <hamiddot47@icloud.com>
2022-07-24 20:35:47 +0500

    Merging two arrays

--- Problem-Solving/merging_arrays.cpp
+++ Problem-Solving/merging_arrays.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main()
 {
-    int target[10];
-    int arr1[]={2,4,6,8,10};
-    int arr2[]={1,3,5,7,9};
-    for (int i = 0; i < 5; i++)
+    int i,j,k,temp;
+    int arr1[] = {2, 4, 6, 8, 10};
+    int arr2[] = {1, 3, 5, 7, 9};
+    int merge[10];
+    for (i = 0; i < 5; i++)
     {
-        
-        cout<<arr1[i]<<" ";
-        
+      merge[i]=arr1[i];  
+    }    
+    k=i;
+    for (i = 0; i < 5; i++)
+    {
+      merge[k]=arr2[i];
+      k++;  
     }
-            cout<<endl;
-        for (int j = 0; j < 5; j++)
-        {
-            cout<<arr2[j]<<" ";
-        }
-        for (int k = 0; k < 10; k++)
-        {
-            target[k]=arr1[]+arr2[];
-        }
-        
-
+    cout<<"Megred array: "<<endl;
+    for (i = 0; i < k; i++)
+    {
+        cout<<merge[i]<<" ";
+    }
     
 
+    
 
     return 0;
 }
