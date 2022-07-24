aaeb2172a0ede1d7edcf13a748985826d37e1982
hamidhere <hamiddot47@icloud.com>
2022-07-24 17:54:24 +0500

    printing odd numbers

---
+++ Problem-Solving/merging_arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int target[10];
+    int arr1[]={2,4,6,8,10};
+    int arr2[]={1,3,5,7,9};
+    for (int i = 0; i < 5; i++)
+    {
+        
+        cout<<arr1[i]<<" ";
+        
+    }
+            cout<<endl;
+        for (int j = 0; j < 5; j++)
+        {
+            cout<<arr2[j]<<" ";
+        }
+        for (int k = 0; k < 10; k++)
+        {
+            target[k]=arr1[]+arr2[];
+        }
+        
+
+    
+
+
+    return 0;
+}
