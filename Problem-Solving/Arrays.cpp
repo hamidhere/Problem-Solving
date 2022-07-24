f52c5b305501fa275a56362ad973961803d63b7b
hamidhere <hamiddot47@icloud.com>
2022-07-24 16:44:03 +0500

    printing odd numbers

--- Problem-Solving/Arrays.cpp
+++ Problem-Solving/Arrays.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int main()
 {
     int size;
-    int num[size] = {1,2,3,4,5,6,7,8,9,10};
-    for (int i = 0; i < size; i++)
-   {
-       cout<<num[size]<<" ";
-   }
-
+    int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < 10; i++)
+    {
+        //  if (num[i]%2==0) //for even numbers
+        if (num[i] % 2 != 0) // for odd numbers
+        {
+            /* code */
+            cout << num[i] << " ";
+        }
+    }
 
     return 0;
 }
